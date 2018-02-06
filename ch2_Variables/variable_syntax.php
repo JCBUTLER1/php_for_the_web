4f8990405b8b8a42ca209b12f5c687dda1eb42f6
Jerrodo <jerrodo.butler@gmail.com>
2018-02-06 07:57:00 -0800

    Added syntax example

---
+++ ch2_Variables/variable_syntax.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    
+  </head>
+  <body>
+       
+       <!-- Chapt 2 VARIABLES:
+          
+           This script declares four variables (three string values and one integer value).
+           A string is printed to the scrren concatenating the variables. 
+           
+       -->
+        <?php
+            $street = "13543 Zinnia Hills Pl., Apt 89"; /* Declare a variable as a string for the street value */
+            $city = "San Diego"; /* Declare a variable as a string for the city value */
+            $state = "CA"; /* Declare a variable as a string for the state value */
+            $zip = 92130; /* Declare a variable for the zip code value */           
+            
+            /* Print to the page an HTML paragraph with the variables to form a statement using their declared values */
+            print "<p>My address is:<br> 
+                    $street<br>
+                    $city $state $zip</p>";
+        ?>
+  </body>
+</html>
